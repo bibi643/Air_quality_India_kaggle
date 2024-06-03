5698ca159c669168bb8758eba6c04404e6ea3053
j <julien_biscay@hotmail.com>
2024-06-03 21:16:44 +0200

    update

--- air_qual.py
+++ air_qual.py
@@ -11,6 +11,7 @@ from prophet import Prophet
 from prophet.diagnostics import cross_validation,performance_metrics
 from sklearn.metrics import mean_absolute_error
 import streamlit as st
+import plotly.figure_factory as ff
 # add streamlit
 
 st.title('Air Quality in India')
@@ -21,6 +22,13 @@ df=df.drop(labels='To Date',axis=1) # we drop one date column
 
 
 df.head()
+# Cut from 2018 to have more consistent data
+df=df['2018-08':]
+
+# Separation in different categories
+part_matters= df.filter(regex='^PM')
+n_molecules= df.filter(regex='^N')
+carbon_o=df.filter(regex='^C')
 
 add_selectbox = st.sidebar.selectbox(
     
@@ -31,13 +39,26 @@ add_selectbox = st.sidebar.selectbox(
 # List of the column we can eventually forecast
 
 
-# Data Viz
+# Data Analasis
 
-if add_selectbox =='Data Visualisation':
-    columns=st.selectbox()
-    if columns:
-        df.columns
-
+## Lineplot
+st.line_chart(df)
+## Histplot
 
 
 
+
+
+#columns name
+col_list=df.columns
+
+col_selection= st.selectbox(
+    'Which Column(s) you would like to check the distribution?',
+    list(df.columns)
+)
+'Your choice is:', col_selection
+group_labels=[col_selection]
+fig= ff.create_distplot([df[col_selection]],group_labels)
+
+
+# Resample, daily, weekly, monthly. give the option. To take ouy the hour.
